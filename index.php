533b52f8b864007760b09e891fb842837715fa9d
arunkp03 <39460801+arunkp03@users.noreply.github.com>
2018-05-20 20:26:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,19 +2,7 @@
 
 
 $aa  = array (
-  'fulfillmentText' => 'This is a text response',
-  'fulfillmentMessages' => 
-  array (
-    0 => 
-    array (
-      'card' => 
-      array (
-        'title' => 'Speed Check',
-        'subtitle' => 'Displays your current speed',
-        'imageUri' => 'https://plus.google.com/_/focus/photos/private/AIbEiAIAAAAiCMr8hcvG19iDRRCwgMnm9NKVl7UBGOTlmODmyM-OlgEwAWLoSrQZkl5cE0L7ZzuftnRR-Zjd?sz=128'       
-      ),
-    ),
-  ),
+  'fulfillmentText' => 'You are very near to the next car.',
   'source' => 'example.com',
   'payload' => 
   array (
