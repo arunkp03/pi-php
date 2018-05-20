835a0a5cabd2910468ca81f272dc91be02d97a2a
arunkp03 <39460801+arunkp03@users.noreply.github.com>
2018-05-20 19:44:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
 
-echo "{
 
-        "speech": "It is very near",
-
-	        "displayText": "It is very near",
-
-		        "source": "speedCheck"
-			}";
-			?>
+$aa  = array("speech"=>"It is very near",
+	     "displayText"=> "It is very near",
+	     "source"=> "speedCheck"
+	    );	
+echo json_encode($aa);
+?>
